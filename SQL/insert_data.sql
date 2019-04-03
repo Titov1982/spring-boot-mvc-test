1c8a75395c9dfcf574b0bdcd2b72ca5c4cf4a32f
Titov Andrey <automatic.spb@mail.ru>
2019-04-03 09:01:28 +0300

    Подготовлен SQL-скрипт для стартового заполнения таблиц пользователей и ролей при разворачивании приложения.

--- SQL/insert_data.sql
+++ SQL/insert_data.sql
@@ -1,13 +1,14 @@
 
-INSERT INTO usr (active, email, first_name, last_name, login, password) values (true, 'titov@mail.ru', 'Andrey', 'Titov', 'titov', '123qwe');
-INSERT INTO usr (active, email, first_name, last_name, login, password) values (true, 'asd@mail.ru', 'Asd', 'Dsa', 'asd', '456qwe');
+INSERT INTO usr (active, email, first_name, last_name, login, password) values (true, 'admin@admin.net', 'Admin', 'Admin', 'admin', 'admin');
+INSERT INTO usr (active, email, first_name, last_name, login, password) values (true, 'user@user.net', 'User', 'User', 'user', 'user');
 
 
-INSERT INTO role (role) values ('USER_R');
 INSERT INTO role (role) values ('ADMIN_R');
+INSERT INTO role (role) values ('USER_R');
 
-INSERT INTO user_role (user_id, role_id) values (1, 2);
-INSERT INTO user_role (user_id, role_id) values (2, 1);
+
 INSERT INTO user_role (user_id, role_id) values (1, 1);
+INSERT INTO user_role (user_id, role_id) values (1, 2);
+INSERT INTO user_role (user_id, role_id) values (2, 2);
 
 
